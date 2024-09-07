15a8f8159c59ddf31c2f8a0334f41a8774dbbb4c
himansuhs <himansusekhar2002@gmail.com>
2024-09-07 16:17:07 +0530

    final upadte

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const port = 4000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 // routes
 app.use("/api/users", authRoutes);
