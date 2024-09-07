47f3b124423ecd7b402ed6619cecb652011207a1
himansuhs <himansusekhar2002@gmail.com>
2024-09-07 12:18:45 +0530

    db update

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    mongoose.connect(process.env.MONGO_URI);
+    mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true, // Add SSL configuration
+    });
     console.log("database connection established");
   } catch (error) {
     console.log(error);
