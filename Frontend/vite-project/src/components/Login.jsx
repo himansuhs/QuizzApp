ef235fa120f2dfda82c82b3062169c79244aa4c8
himansuhs <himansusekhar2002@gmail.com>
2024-09-07 16:32:41 +0530

    frontend update

--- Frontend/vite-project/src/components/Login.jsx
+++ Frontend/vite-project/src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:4000/api/users/login",
+        "https://quizzapp-ard5.onrender.com/api/users/login",
         formData
       );
 
